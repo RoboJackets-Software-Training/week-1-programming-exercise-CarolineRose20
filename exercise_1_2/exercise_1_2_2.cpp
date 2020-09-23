7e85c304d0d57dc52a8c5f73d49adf95ca0b200b
CarolineRose20 <71110324+CarolineRose20@users.noreply.github.com>
2020-09-23 13:58:34 -0400

    Update exercise_1_2_2.cpp

--- exercise_1_2/exercise_1_2_2.cpp
+++ exercise_1_2/exercise_1_2_2.cpp
@@ -11,6 +11,24 @@ int main()
 
     // Palindrome Check
     // WRITE YOUR PALINDROME CHECK HERE
+    int left = 0;
+    int right = str.length() - 1;
+    bool is_palindrome = true;
+    while (left < right) {
+    	if (str[left] != str[right]) {
+    		is_palindrome = false;
+    		break;
+    	}
+    	std::cout << is_palindrome;
+    	left++;
+    	right--;
+    }
+
+    if (is_palindrome == 1) {
+    	std::cout << str << " is a palindrome" << std::endl;
+    } else {
+    	std::cout << str << " is NOT a palindrome" << std::endl;
+    }
 
     return 0;
 }
